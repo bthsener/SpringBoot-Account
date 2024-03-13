6ebedf9a379ae9ac3371db684e86cb5eade6a978
abatuhan06 <abatuhan0666@gmail.com>
2024-03-13 16:25:45 +0300

    resources added

---
+++ src/main/java/com/batuhansener/account/AccountApplication.java
@@ -0,0 +1,24 @@
+package com.batuhansener.account;
+
+import com.batuhansener.account.model.Customer;
+import com.batuhansener.account.repository.CustomerRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashSet;
+
+@SpringBootApplication
+public class AccountApplication {
+
+	private final CustomerRepository customerRepository;
+
+	public AccountApplication(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+}
