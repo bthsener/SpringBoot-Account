f7c35175a25ab8a2dc5299ab0c414d69d9b77bf8
abatuhan06 <abatuhan0666@gmail.com>
2024-03-13 11:09:12 +0300

    models added

---
+++ src/main/java/com/batuhansener/account/model/Customer.kt
@@ -0,0 +1,42 @@
+package com.batuhansener.account.model
+
+import jakarta.persistence.*
+import org.hibernate.annotations.GenericGenerator
+import java.time.LocalDateTime
+
+@Entity
+data class Customer (
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    val id: String?,
+    val name: String?,
+    val surname: String?,
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    val accounts: Set<Account>?
+) {
+    constructor() : this(null, null, null, null)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Customer
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (surname != other.surname) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + (name?.hashCode() ?: 0)
+        result = 31 * result + (surname?.hashCode() ?: 0)
+        return result
+    }
+
+
+}
