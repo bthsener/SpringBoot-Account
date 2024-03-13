5bf7828f37e6ab68440bc0ff3bc2472a30393224
abatuhan06 <abatuhan0666@gmail.com>
2024-03-13 16:24:41 +0300

    controller added

---
+++ src/main/java/com/batuhansener/account/controller/AccountContoller.java
@@ -0,0 +1,29 @@
+package com.batuhansener.account.controller;
+
+import com.batuhansener.account.dto.AccountDto;
+import com.batuhansener.account.dto.CreateAccountRequest;
+import com.batuhansener.account.model.Account;
+import com.batuhansener.account.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/account")
+public class AccountContoller {
+    private final AccountService accountService;
+
+
+    public AccountContoller(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountDto> createAccount(@RequestBody CreateAccountRequest request){
+        return ResponseEntity.ok(accountService.createAccount(request));
+    }
+
+    @GetMapping
+    public String getMessage(){
+        return "selam";
+    }
+}
