2b1a5237ddf85e56e5c14f943039465b014cdcf2
abatuhan06 <abatuhan0666@gmail.com>
2024-03-14 10:35:28 +0300

    customerAccountDtoConverter added

---
+++ src/main/java/com/batuhansener/account/dto/converter/CustomerAccountDtoConverter.java
@@ -0,0 +1,29 @@
+package com.batuhansener.account.dto.converter;
+
+import com.batuhansener.account.dto.CustomerAccountDto;
+import com.batuhansener.account.model.Account;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Component
+public class CustomerAccountDtoConverter {
+
+    private final TransactionDtoConverter transactionDtoConverter;
+
+    public CustomerAccountDtoConverter(TransactionDtoConverter transactionDtoConverter) {
+        this.transactionDtoConverter = transactionDtoConverter;
+    }
+
+    public CustomerAccountDto convert(Account from) {
+        return new CustomerAccountDto(
+                from.getId(),
+                from.getBalance(),
+                from.getTransaction()
+                        .stream()
+                        .map(transactionDtoConverter::convert)
+                        .collect(Collectors.toSet()),
+                from.getCreationDate());
+    }
+}
--- src/main/java/com/batuhansener/account/dto/converter/CustomerDtoConverter.java
+++ src/main/java/com/batuhansener/account/dto/converter/CustomerDtoConverter.java
@@ -1,11 +1,20 @@
 package com.batuhansener.account.dto.converter;
 
 import com.batuhansener.account.dto.AccountCustomerDto;
+import com.batuhansener.account.dto.CustomerDto;
 import com.batuhansener.account.model.Customer;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Component
 public class CustomerDtoConverter {
+
+    private final CustomerAccountDtoConverter customerAccountDtoConverter;
+
+    public CustomerDtoConverter(CustomerAccountDtoConverter converter) {
+        this.customerAccountDtoConverter = converter;
+    }
 
     public AccountCustomerDto convertToAccountCustomer(Customer from){
         if (from==null){
@@ -14,4 +23,15 @@ public class CustomerDtoConverter {
         return new AccountCustomerDto(from.getId(), from.getName(), from.getSurname());
     }
 
+    public CustomerDto convertToCustomerDto(Customer from) {
+        return new CustomerDto(
+                from.getId(),
+                from.getName(),
+                from.getSurname(),
+                from.getAccounts()
+                        .stream()
+                        .map(customerAccountDtoConverter::convert)
+                        .collect(Collectors.toSet()));
+
+    }
 }
