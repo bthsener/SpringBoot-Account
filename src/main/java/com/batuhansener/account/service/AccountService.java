3fd36dca1a401453098747cb20dfb2bda9094f52
abatuhan06 <abatuhan0666@gmail.com>
2024-03-15 10:27:21 +0300

    delete account added

--- src/main/java/com/batuhansener/account/service/AccountService.java
+++ src/main/java/com/batuhansener/account/service/AccountService.java
@@ -3,6 +3,7 @@ package com.batuhansener.account.service;
 import com.batuhansener.account.dto.AccountDto;
 import com.batuhansener.account.dto.CreateAccountRequest;
 import com.batuhansener.account.dto.converter.AccountDtoConverter;
+import com.batuhansener.account.exception.AccountNotFoundException;
 import com.batuhansener.account.model.Account;
 import com.batuhansener.account.model.Customer;
 import com.batuhansener.account.model.Transaction;
@@ -32,12 +33,27 @@ public class AccountService {
     public AccountDto createAccount(CreateAccountRequest createAccountRequest){
         Customer customer = customerService.findCustomerById(createAccountRequest.getCustomer_id());
         Account account = new Account(customer, createAccountRequest.getInitialCredit(), LocalDateTime.now());
-        account = accountRepository.save(account);
+//        account = accountRepository.save(account);
         if (createAccountRequest.getInitialCredit().compareTo(BigDecimal.ZERO) > 0) {
-            Transaction transaction = transactionService.initiateMoney(account, createAccountRequest.getInitialCredit());
+//            Transaction transaction = transactionService.initiateMoney(account, createAccountRequest.getInitialCredit());
+            Transaction transaction = new Transaction(
+                    createAccountRequest.getInitialCredit(),
+                    LocalDateTime.now(),
+                    account
+                    );
             account.getTransaction().add(transaction);
         }
-        return converter.convert(account);
+        return converter.convert(accountRepository.save(account));
     }
 
+    public Account findAccountById(String id){
+        System.out.println("şimdi de burada");
+        return accountRepository.findById(id).orElseThrow(()-> new AccountNotFoundException("Account could not find by id "+id));
+    }
+
+    public void deleteAccount(String id){
+        Account account = findAccountById(id);
+        transactionService.deleteAllAccountTransactions(account.getTransaction());
+        accountRepository.delete(account);
+    }
 }
